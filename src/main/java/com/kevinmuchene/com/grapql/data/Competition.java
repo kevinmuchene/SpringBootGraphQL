a09234bc4f3a552eb4f71a8c73fecda72301bebb
Kevin Muchene <muchenekevin@gmail.com>
2023-05-31 19:57:56 -0500

    Generated constructor manually to handle parameters passed. Also, used @JoinTable to handle @ManyToMany association

--- src/main/java/com/kevinmuchene/com/grapql/data/Competition.java
+++ src/main/java/com/kevinmuchene/com/grapql/data/Competition.java
@@ -3,6 +3,8 @@ package com.kevinmuchene.com.grapql.data;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -27,4 +29,21 @@ public class Competition {
 
     @Column(name = "NUMBER_OF_TEAMS")
     private int numberOfTeams;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+            name = "competition_team",
+            joinColumns = @JoinColumn(name = "competition_id"),
+            inverseJoinColumns = @JoinColumn(name = "team_id")
+    )
+    private List<Team> listOfTeams;
+
+
+    public Competition(Long id, String name, float price, String country, int numberOfTeams) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.country = country;
+        this.numberOfTeams = numberOfTeams;
+    }
 }
